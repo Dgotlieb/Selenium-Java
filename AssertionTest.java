d75221904d0f893b4f33059abc8076de1dd33f7b
dgotlieb <Dgotlieb8@gmail.com>
2019-07-20 20:41:26 +0300

    Add files via upload

---
+++ AssertionTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AssertionTest {
+
+    @Test
+    public void test_method_1() {
+        int x = 7;
+        int y = 5;
+        assertEquals(x,y);
+    }
+}

f12f8e44744dc18650b02b1f19668861d83eec3b
dgotlieb <Dgotlieb8@gmail.com>
2019-07-20 20:47:51 +0300

    Add files via upload

---
+++ OptionsTest.java
@@ -0,0 +1,30 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class OptionsTest {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void beforeAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\XXXXXXXXXX\\Downloads\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("-incognito");
+        driver = new ChromeDriver(options);
+        driver.get("https://translate.google.com");
+    }
+
+    @Test
+    public void test_01(){
+        System.out.println(driver.getTitle());
+    }
+
+    @AfterClass
+    public static void afterAll(){
+        driver.quit();
+    }
+}

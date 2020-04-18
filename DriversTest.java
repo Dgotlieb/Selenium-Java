67cd4dfd1857b3768a02776fbbe8f7c1b72a3996
dgotlieb <Dgotlieb8@gmail.com>
2020-04-18 20:48:25 +0300

    Create DriversTest.java

---
+++ DriversTest.java
@@ -0,0 +1,18 @@
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriversTest{
+
+    @BeforeClass
+    public static void runOnceBeforeClass() {
+        System.setProperty("webdriver.chrome.driver" , "C://…..ChromeDriver.exe");
+        WebDriver driver = new ChromeDriver();
+        // Get driver at: https://chromedriver.chromium.org/downloads
+
+        System.setProperty("webdriver.gecko.driver","C://..geckodriver.exe");
+        WebDriver fireFoxDriver = new FirefoxDriver();
+        // Get driver at: https://github.com/mozilla/geckodriver/releases
+    }
+}

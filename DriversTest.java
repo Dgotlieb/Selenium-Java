16dc748e3cb6d231135b70371042753f06845b74
dgotlieb <Dgotlieb8@gmail.com>
2020-07-16 20:57:51 +0300

    Update DriversTest.java

--- DriversTest.java
+++ DriversTest.java
@@ -1,4 +1,4 @@
-import org.junit.BeforeClass;
+import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;

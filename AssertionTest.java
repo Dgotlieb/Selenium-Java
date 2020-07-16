fdfa4487e6e27ec65f78dcafed8b524a174b6c0f
dgotlieb <Dgotlieb8@gmail.com>
2020-07-16 21:11:35 +0300

    Update AssertionTest.java

--- AssertionTest.java
+++ AssertionTest.java
@@ -1,6 +1,5 @@
-
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AssertionTest {
 
@@ -8,6 +7,6 @@ public class AssertionTest {
     public void test_method_1() {
         int x = 7;
         int y = 5;
-        assertEquals(x,y);
+        Assert.assertEquals(x,y);
     }
 }

d36caae9d910c28b676b05ada3da7f88f1af9468
dgotlieb <Dgotlieb8@gmail.com>
2020-07-16 21:09:53 +0300

    Update AssertionAndVerification.java

--- AssertionAndVerification.java
+++ AssertionAndVerification.java
@@ -1,8 +1,6 @@
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runners.MethodSorters;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
